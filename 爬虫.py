69aaa0102f48af66453e45a5ee0a5aedd2b41fd3
bondli <1020120202@qq.com>
2025-04-04 08:43:06 +0800

    Delete 爬虫.py

--- 爬虫.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import urllib.parse 
-
-
-data = bytes(urllib.parse.urlencode({'word':'hello'}),encoding='utf8')
-response = urllib.request.urlopen('http://httpbin.org/post',data=data)
-html =response.read()
-print(html)
